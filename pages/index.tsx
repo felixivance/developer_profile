087ca52dcbbc5ca23cb7b70301e88108ef86401d
Felix <felixrunye@gmail.com>
2022-02-14 16:57:27 +0300

    updated header text

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,11 @@ export default function Home() {
       </Head>
   
   {/* nav */}
-      <div className='flex items-center justify-between pl-3 pr-3'>
+      <div className='flex items-center justify-between p-3'>
         {/* left  */}
-        <div className=''>
-          <Image src="/assets/images/jp_morgan.svg" alt="Jonathan Specter logo" className="w-10 h-10" width={100} height={70} />
+        <div className='text-white'>
+          {/* <Image src="/assets/images/jp_morgan.svg" alt="Jonathan Specter logo" className="w-10 h-10" width={100} height={70} /> */}
+          <p className='text-xl'>Johnathan Specter</p>
         </div>
           
         {/* right */}
@@ -21,7 +22,16 @@ export default function Home() {
           <Image src="/assets/images/hamburger.svg" alt="Menu" className="w-10 h-10" width={50} height={20} />
         </div>
       </div>
-    
+
+{/*     Main banner */}
+      <section className='pl-3 pr-3'>
+        <div className='text-white'>
+          <p className='text-4xl'>Helping Companies build better, scalable software.</p>
+
+          <p>Award-winning web developer and author, with over 15+ years of working experience with 
+            Fortune 500 companies like Apple, Google, Facebook and more. </p>
+        </div>
+      </section>
     </div>
   )
 }
